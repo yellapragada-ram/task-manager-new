5add8591616b9c53357e2bfe6a55482a6b399d12
deepak <deepak.example@gmail.com>
2023-12-14 10:15:37 +0530

    commiting changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'task-manager';
+  selectedDate: Date | null = null; // Initialize as null
+  daysOfWeek: string[] = ['Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
+
+  setSelectedDate(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.selectedDate = inputElement.valueAsDate;
+  }
+
+  getDayFromDate(): number | null {
+    return this.selectedDate ? this.selectedDate.getDay() : null;
+  }
+  newItemTitle: string = '';
+  newItemDescription: string = '';
+  items: { title: string, description: string }[] = [];
+
+  addItem() {
+    if (this.newItemTitle && this.newItemDescription) {
+      this.items.push({
+        title: this.newItemTitle,
+        description: this.newItemDescription
+      });
+      this.newItemTitle = '';
+      this.newItemDescription = '';
+    }
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
 }
